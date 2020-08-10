728a308dc02104fedad7b1d4dc918d441139e601
admin <admin@admin-PC>
2020-08-10 22:37:33 +0530

    Starter process change

--- merchant-company/src/main/java/com/harishtest/merchantdetails/merchantcompany/MerchantCompanyStarter.java
+++ merchant-company/src/main/java/com/harishtest/merchantdetails/merchantcompany/MerchantCompanyStarter.java
@@ -2,17 +2,25 @@ package com.harishtest.merchantdetails.merchantcompany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties
-public class MerchantCompanyStarter {
+public class MerchantCompanyStarter extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 	
 		//Starter service
 		SpringApplication.run(MerchantCompanyStarter.class, args);
 
+	}
+	
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+	{
+		return builder.sources(MerchantCompanyStarter.class);
 	}
 
 }
