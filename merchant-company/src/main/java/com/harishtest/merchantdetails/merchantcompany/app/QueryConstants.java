e2a6f648c82962facd15f7d996fb34d335a699cb
admin <admin@admin-PC>
2020-08-12 01:56:21 +0530

    querychange

--- merchant-company/src/main/java/com/harishtest/merchantdetails/merchantcompany/app/QueryConstants.java
+++ merchant-company/src/main/java/com/harishtest/merchantdetails/merchantcompany/app/QueryConstants.java
@@ -2,10 +2,10 @@ package com.harishtest.merchantdetails.merchantcompany.app;
 
 public interface QueryConstants {
 	
-	String InsertQuery="INSERT INTO Merchant.MERCHANTRECORDS (ACCHOLDERNAME,MerchantID,LastName,FirstName,AddressLine1,AddressLine2,Phoneno,Pincode,EmailId,CreateDateTime,BANKAccountNumber,CITY,STATE,COUNTRY,PHONETYPE,DOB,BANKNAME,IFSC) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-	String UpdateSEQID="UPDATE Merchant.sequence SET id=LAST_INSERT_ID(id+1)";
+	String InsertQuery="INSERT INTO MERCHANT.MERCHANTRECORDS (ACCHOLDERNAME,MerchantID,LastName,FirstName,AddressLine1,AddressLine2,Phoneno,Pincode,EmailId,CreateDateTime,BANKAccountNumber,CITY,STATE,COUNTRY,PHONETYPE,DOB,BANKNAME,IFSC) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+	String UpdateSEQID="UPDATE MERCHANT.sequence SET id=LAST_INSERT_ID(id+1)";
 	String QuerySEQID="SELECT LAST_INSERT_ID()";
-	String QuerybyId="SELECT ACCHOLDERNAME,MerchantID,LastName,FirstName,AddressLine1,AddressLine2,Phoneno,Pincode,EmailId,BANKAccountNumber,CITY,STATE,COUNTRY,PHONETYPE,DOB,BANKNAME,IFSC FROM MERCHANT.MERCHANTRECORDS WHERE MerchantID=?";
-	String QueryAllbyphone="SELECT ACCHOLDERNAME,MerchantID,LastName,FirstName,AddressLine1,AddressLine2,Phoneno,Pincode,EmailId,BANKAccountNumber,CITY,STATE,COUNTRY,PHONETYPE,DOB,BANKNAME,IFSC FROM MERCHANT.MERCHANTRECORDS WHERE  Phoneno=?";
-	String Querybyphone="SELECT MerchantID,BANKAccountNumber FROM MERCHANT.MERCHANTRECORDS WHERE Phoneno=? AND BANKAccountNumber=? ";
+	String QuerybyId="SELECT ACCHOLDERNAME,MerchantID,LastName,FirstName,AddressLine1,AddressLine2,Phoneno,Pincode,EmailId,BankAccountNumber,City,State,Country,PHONETYPE,DOB,BankName,IFSC FROM MERCHANT.MERCHANTRECORDS WHERE MerchantID=?";
+	String QueryAllbyphone="SELECT ACCHOLDERNAME,MerchantID,LastName,FirstName,AddressLine1,AddressLine2,Phoneno,Pincode,EmailId,BankAccountNumber,City,State,Country,PHONETYPE,DOB,BankName,IFSC FROM MERCHANT.MERCHANTRECORDS WHERE  Phoneno=?";
+	String Querybyphone="SELECT MerchantID,BankAccountNumber FROM MERCHANT.MERCHANTRECORDS WHERE Phoneno=? AND BANKAccountNumber=? ";
 }
