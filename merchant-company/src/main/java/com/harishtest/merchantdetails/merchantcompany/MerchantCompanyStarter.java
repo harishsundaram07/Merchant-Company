abe2c2330057c1450a9fdb1b18fcb4299b0b2ced
admin <admin@admin-PC>
2020-08-12 16:10:58 +0530

    main

--- merchant-company/src/main/java/com/harishtest/merchantdetails/merchantcompany/MerchantCompanyStarter.java
+++ merchant-company/src/main/java/com/harishtest/merchantdetails/merchantcompany/MerchantCompanyStarter.java
@@ -2,24 +2,15 @@ package com.harishtest.merchantdetails.merchantcompany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MerchantCompanyStarter extends SpringBootServletInitializer {
+public class MerchantCompanyStarter  {
 
 	public static void main(String[] args) {
 	
 		//Starter service
 		SpringApplication.run(MerchantCompanyStarter.class, args);
 
-	}
-	
-	@Override
-	public SpringApplicationBuilder configure(SpringApplicationBuilder builder)
-	{
-		return builder.sources(MerchantCompanyStarter.class);
 	}
 
 }
